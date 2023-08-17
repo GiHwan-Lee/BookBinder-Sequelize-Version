6f580f711b327dbbf40fb305ad3ee0e425acbbfd
Gi Hwan <ass302@naver.com>
2023-08-17 20:32:01 +0900

    Refactor remaining functions in book data layer for Sequelize integration

--- data/bookModel.js
+++ data/bookModel.js
@@ -74,3 +74,61 @@ export async function getByCategory(categoryId) {
     books: rows,
   };
 }
+
+export async function getByTitle(bookName) {
+  return Books.findOne({ where: { bookTitle: bookName } });
+}
+
+export async function getByPublisher(publisherName) {
+  const { count, rows } = await Books.findAndCountAll({
+    where: { publisher: publisherName },
+  });
+  return {
+    total: count,
+    books: rows,
+  };
+}
+
+export async function getTotalSalesByCategoryId(categoryId) {
+  const totalSales = await Books.sum("salesQuantity", {
+    where: { categoryId },
+  });
+  return totalSales || null;
+}
+
+export async function getTotalStockByCategoryId(categoryId) {
+  const totalStock = await Books.sum("productCount", {
+    where: { categoryId },
+  });
+  return totalStock || null;
+}
+
+export async function getTopThree() {
+  return Books.findAll({
+    order: [["salesQuantity", "DESC"]],
+    limit: 3,
+  });
+}
+
+export async function updateSalesQuantity(bookName, publicationYear, newSales) {
+  const [affectedCount] = await Books.update(
+    { salesQuantity: newSales },
+    { where: { bookTitle: bookName, publicationYear } }
+  );
+  return affectedCount > 0;
+}
+
+export async function updateProductCount(bookName, publicationYear, newStock) {
+  const [affectedCount] = await Books.update(
+    { productCount: newStock },
+    { where: { bookTitle: bookName, publicationYear } }
+  );
+  return affectedCount > 0;
+}
+
+export async function deleteBookByTitleAndYear(bookName, publicationYear) {
+  const affectedCount = await Books.destroy({
+    where: { bookTitle: bookName, publicationYear },
+  });
+  return affectedCount > 0;
+}
